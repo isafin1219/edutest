ad3a562224dee92bc6d3fb5f3a4bc95691482de7
Ilnur Safin <isafin1219@gmail.com>
2020-04-08 17:35:33 +0400

    08APR 1735

---
+++ polls/test.py
@@ -0,0 +1,14 @@
+import datetime
+from unittest import TestCase
+
+from django.utils import timezone
+
+from polls.models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
